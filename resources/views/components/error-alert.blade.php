0b1e7e32a500e9fcfc5830d436cae9342092bc5e
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-07-23 11:24:41 +0300

    refactor(ui): extract error alert into reusable component
    - Add new `error-alert` Blade component for consistent error message styling.

---
+++ resources/views/components/error-alert.blade.php
@@ -0,0 +1,9 @@
+<div
+    {{ $attributes->merge(['class' => 'p-4 flex items-center text-sm text-red-600 border border-red-200 rounded-lg bg-red-50 dark:border-red-800 dark:bg-red-900/50 dark:text-red-400']) }}>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+              clip-rule="evenodd"/>
+    </svg>
+    {{ $slot }}
+</div>
