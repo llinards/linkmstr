d51db7a1d4f7b44d6e8e22e121e156accbbedb7b
Linards Lazdiņš <linards_lazdins@hotmail.com>
2025-07-23 22:02:40 +0300

    refactor(link-manager): enhance expired link handling and UI feedback
    - Add expired state check for links and update status display logic.
    - Disable toggle for expired links in the `flux` switch component.

--- resources/views/livewire/links/link-manager.blade.php
+++ resources/views/livewire/links/link-manager.blade.php
@@ -75,7 +75,7 @@
                             <svg class="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                  fill="currentColor" viewBox="0 0 24 24">
                                 <path
-                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5. пропорц5a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+                                    d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
                             </svg>
                         @endif
                     </div>
@@ -116,11 +116,27 @@
                     </td>
                     <td class="px-6 py-4">
                         <div class="flex flex-col items-start space-y-2">
-                            <span
-                                class="text-xs font-medium {{ $link->is_active ? 'text-green-500' : 'text-red-500' }}">
-                                {{ $link->is_active ? 'Active' : 'Inactive' }}
+                            @php
+                                $isExpired = $link->expires_at && $link->expires_at->isPast();
+                            @endphp
+                            <span class="text-xs font-medium
+                                @if($isExpired)
+                                    text-orange-500
+                                @elseif($link->is_active)
+                                    text-green-500
+                                @else
+                                    text-red-500
+                                @endif">
+                                @if($isExpired)
+                                    Expired
+                                @elseif($link->is_active)
+                                    Active
+                                @else
+                                    Inactive
+                                @endif
                             </span>
-                            <flux:switch :checked="$link->is_active"
+                            <flux:switch :checked="$link->is_active && !$isExpired"
+                                         :disabled="$isExpired"
                                          wire:click="toggleActive({{ $link->id }})"/>
                         </div>
                         <flux:error name="links.{{ $link->id }}.is_active"/>
